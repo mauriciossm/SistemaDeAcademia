bdf60064e66d709fb32a9cb736e7ed31b59a6607
Mauricio Magalhaes <mauriciosmagalhaes@gmail.com>
2023-03-09 12:01:45 -0300

    Primeira parte front end

---
+++ SistemaDeAcademia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaDeAcademia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SistemaDeAcademia/Models/InstituicaoModel.cs
@@ -0,0 +1,20 @@
+﻿using SistemaDeAcademia.Enum;
+
+namespace SistemaDeAcademia.Models
+{
+    public class InstituicaoModel
+    {
+        public int Id { get; set; }
+        public string NomePropietario { get; set; }
+        public string CPF { get; set; }
+        public string NomeInstituicao { get; set; }
+        public EstadoEnum Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string Senha { get; set; }
+    }
+}
